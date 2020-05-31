994546abdc5a7dfb23556f4fd3470c3c3dc69bac
dep-vol <alex.tecto.msk@gmail.com>
2020-05-31 04:13:42 +0300

    Updates

---
+++ precache-manifest.43c8e71de2b1decf497165a72d7f5a2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8032d268d3ac8fbeb972015c368b0ce6",
+    "url": "/PokeBattle/index.html"
+  },
+  {
+    "revision": "08f4809e88c584cb5667",
+    "url": "/PokeBattle/static/css/main.9b69bb5c.chunk.css"
+  },
+  {
+    "revision": "801a642c839d5df49e57",
+    "url": "/PokeBattle/static/js/2.ce5f8db5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/PokeBattle/static/js/2.ce5f8db5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08f4809e88c584cb5667",
+    "url": "/PokeBattle/static/js/main.e9ac0071.chunk.js"
+  },
+  {
+    "revision": "defe43d19ebed4676b9f",
+    "url": "/PokeBattle/static/js/runtime-main.12d48dc3.js"
+  },
+  {
+    "revision": "40fac573903750616b7375355b7a2329",
+    "url": "/PokeBattle/static/media/pokemon_logo.40fac573.png"
+  }
+]);
